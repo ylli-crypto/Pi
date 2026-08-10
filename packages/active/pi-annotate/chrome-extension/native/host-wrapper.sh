2edbae54b19d58d6223eca28a154e43f373eefdd
Ylli Kolgeci <ylli-crypto@users.noreply.github.com>
2026-08-10 16:03:36 +0200

    Fix pi-annotate native host wrapper path to match setup location

--- packages/active/pi-annotate/chrome-extension/native/host-wrapper.sh
+++ packages/active/pi-annotate/chrome-extension/native/host-wrapper.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-exec "/opt/homebrew/bin/node" "/Users/ylli/.pi/agent/local/pi-annotate/chrome-extension/native/host.cjs" "$@"
+exec "/opt/homebrew/bin/node" "/Users/ylli/Pi/packages/active/pi-annotate/chrome-extension/native/host.cjs" "$@"
